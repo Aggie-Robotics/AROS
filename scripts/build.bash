6270885fd0a5622f3e1b7e78aba10b47e141e57a
buzzec <buzzec@buzzec.net>
2021-01-17 22:12:50 -0600

    Fixed script features

--- scripts/build.bash
+++ scripts/build.bash
@@ -2,6 +2,6 @@
 
 set -e
 
-cargo +nightly build --manifest-path ../Cargo.toml --target armv7a-none-eabi --release
+cargo +nightly build --manifest-path ../Cargo.toml --target armv7a-none-eabi --release --package v5_rust
 cp ../target/armv7a-none-eabi/release/libv5_rust.a ../pros_package/firmware/
 prosv5 build --project ../pros_package
--- scripts/test.bash
+++ scripts/test.bash
@@ -2,7 +2,7 @@
 
 set -e
 
-cargo +nightly build --manifest-path ../Cargo.toml --target armv7a-none-eabi --release --features v5_test
+cargo +nightly build --manifest-path ../Cargo.toml --target armv7a-none-eabi --release --features v5_test --package v5_rust
 cp ../target/armv7a-none-eabi/release/libv5_rust.a ../pros_package/firmware/
 prosv5 build --project ../pros_package
 prosv5 upload --project ../pros_package --name aros_test --slot 8
