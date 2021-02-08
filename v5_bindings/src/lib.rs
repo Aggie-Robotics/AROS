cb7d2381d852f7e5d617fe0fe71c07e5e9f5f645
buzzec <buzzec@buzzec.net>
2021-02-08 17:55:55 -0600

    Added universal functions

--- v5_bindings/src/lib.rs
+++ v5_bindings/src/lib.rs
@@ -15,6 +15,9 @@ use crate::raw::pros::api::printf;
 pub use crate::raw::pros::rtos::notify_action_e_t as NotifyAction;
 pub use crate::raw::pros::rtos::task_state_e_t as State;
 pub use crate::task::Task;
+use v5_traits::{UniversalFunctions, LogLevel};
+use crate::raw::pros::rtos::delay;
+use ansi_rgb::{Foreground, white, Background, red, orange, magenta, blue};
 
 // use std::sync::Mutex;
 
@@ -47,4 +50,39 @@ pub fn console_print(fmt: impl Display){
 }
 pub fn console_println(fmt: impl Display){
     console_print(&format!("{}\n", fmt))
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct V5UniversalFunctions;
+impl UniversalFunctions for V5UniversalFunctions{
+    fn delay(&self, duration: Duration) {
+        unsafe {delay(duration.as_millis() as u32)}
+    }
+
+    fn system_time(&self) -> Duration {
+        system_time()
+    }
+
+    fn print(&self, out: impl Display) {
+        console_print(out)
+    }
+
+    fn eprint(&self, out: impl Display) {
+        console_print(out.fg(white()).bg(red()))
+    }
+
+    fn min_log_level(&self) -> LogLevel {
+        LogLevel::DEBUG
+    }
+
+    fn log_intern(&self, message: impl Display, level: LogLevel) {
+        match level {
+            LogLevel::FATAL => console_print(message.fg(white()).bg(red())),
+            LogLevel::ERROR => console_print(message.fg(red())),
+            LogLevel::WARN => console_print(message.fg(orange())),
+            LogLevel::DEBUG => console_print(message.fg(magenta())),
+            LogLevel::INFO => console_print(message.fg(blue())),
+            LogLevel::TRACE => console_print(message.fg(white())),
+        }
+    }
 }
